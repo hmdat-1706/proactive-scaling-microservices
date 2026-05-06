550dc96dd0ba870aff11f71ff2ced5ed852320e2
hmdat-1706 <hmdat1706@gmail.com>
2026-05-06 19:33:49 +0700

    feat: use static prophet model for ai-server to ensure high availability

--- apps/ai-scaler/ai_server.py
+++ apps/ai-scaler/ai_server.py
@@ -1,23 +1,13 @@
 from fastapi import FastAPI
-import math
 import pandas as pd
 import mlflow.prophet
-from mlflow.tracking import MlflowClient
 import os
 
 app = FastAPI()
-MLFLOW_URI = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow-service.mlops.svc.cluster.local:5000")
-mlflow.set_tracking_uri(MLFLOW_URI)
 PREDICT_MINUTES = int(os.getenv("PREDICT_MINUTES", 15))
 
-client = MlflowClient()
-experiment = client.get_experiment_by_name("Proactive_Scaling")
-runs = client.search_runs(experiment_ids=[experiment.experiment_id], order_by=["start_time DESC"], max_results=1)
-
-latest_run_id = runs[0].info.run_id
-MODEL_URI = f"runs:/{latest_run_id}/prophet_model"
-
-model = mlflow.prophet.load_model(MODEL_URI)
+# Đọc model demo từ thư mục
+model = mlflow.prophet.load_model("./prophet_model")
 
 @app.get("/api/forecast")
 def get_forecast():
